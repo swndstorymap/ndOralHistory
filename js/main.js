5d176c0d7aebfb22ad82aa5d1d3d8b1ef5a60bd3
jake-steinberg <j.steinberg@live.com>
2022-06-29 08:10:47 -0500

    upload of audio

--- js/main.js
+++ js/main.js
@@ -1,20 +1,34 @@
 // Add all scripts to the JS folder
 
-var map;
+let map;
+let pointLayer;
+let geoJson = {
+    type: "FeatureCollection",
+    name: "historyData",
+    features: []
+};
 
 function createMap(){
+    //create basemap
     map = L.map('map', {
         center: [46.6746,-102.855],
-        zoom: 10
+        zoom: 10,
+        minZoom: 10,
+        scrollWheelZoom: false,
+        maxBounds: [
+            [47.084, -103.657],
+            [46.6, -102.046]
+        ]
     });
 
     L.tileLayer('http://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
         attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap contributors</a>'
     }).addTo(map);
 
-    getData(map);
+    getData();
 };
 
+/*
 function createPopupContent(feature){
     var popupContent = "<p><b>Name:</b> " + feature.properties.Loc_Name + 
         "</p><p><b>County:</b> " + feature.properties.Loc_County + 
@@ -27,7 +41,8 @@ function createPopupContent(feature){
     return popupContent
 };
 
-function getData(map){
+
+function getData(){
     //load the data
     fetch("data/NDOralHistory.geojson")
         .then(function(response){
@@ -44,7 +59,66 @@ function getData(map){
             }).addTo(map);
         })  
 };
+*/
 
+//function to retrieve the data and place it on the map
+function getData(){
+    //Papaparse library reads csvs - more info here: https://www.papaparse.com/
+    Papa.parse('data/NDOralHistory.csv', {
+        download: true,
+        header: true,
+        dynamicTyping: true,
+        complete: results => {
+            results.data.forEach(function(data){
+                //creates feature to contain story data
+                let feature = {}
+                feature.type = "Feature";
+                feature.properties = {};
+                feature.geometry = {
+                    type: "Point",
+                    coordinates: [parseFloat(data.Longitude), parseFloat(data.Latitude)]
+                };
+                //for loop turns csv columns into feature properties
+                for (const property in data){
+                    feature.properties[property] = data[property];
+                }
+            //pushes feature into geoJson created at the beginning of the script
+            geoJson.features.push(feature)
+            });
+            //add data to the map
+            addData();
+        }
+    })
+};
+
+//function to add data to the map
+function addData(){
+    pointLayer = L.geoJson(geoJson/*,{
+        onEachFeature:function(feature, layer){
+            return onEachFeature(feature, layer)
+        }
+    }*/)
+    .addTo(map);
+};
+
+//function to bind popups to points
+function onEachFeature(feature, layer){
+    //create new popup content
+    var popupContent = new popUpContent(feature.properties);
+
+    //bind the popup to the  marker    
+    layer.bindPopup(popupContent.formatted);
+
+    //return the marker to the L.geoJson pointToLayer option
+    return layer;
+};
+
+//fucntion to define the pop up content
+function popUpContent(properties){
+    this.properties = properties;
+    this.formatted  = "<p><b>" + properties.History + "</b></p>" + 
+                      "<p style='width: 100%; text-align: center'><a href='" + properties['Permanent Link'] + "'>View Story</a></p>"
+    };
 
 
 document.addEventListener('DOMContentLoaded',createMap)
